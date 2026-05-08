43788eb8c5c79e26f63d29180db8a6d3682a5dc9
Benade2023 <benademabondzo@gmail.com>
2026-05-08 17:27:03 +0200

    ajout des headers

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Origin', 'https://carried-backlight-deftly.ngrok-free.dev');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
 
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
