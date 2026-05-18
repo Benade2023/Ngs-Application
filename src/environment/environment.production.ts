6f9b6c13c6f0eb081201b7c375679a0662096909
Benade2023 <benademabondzo@gmail.com>
2026-05-18 18:18:48 +0200

    up

--- src/environment/environment.production.ts
+++ src/environment/environment.production.ts
@@ -1,10 +1,10 @@
 export const EnvironmentProduction = {
-   //apiUrlExtern: "http://192.168.1.67:3000/api",
-   //apiUrlFile: "http://192.168.1.67:3000",
-   apiUrlFile: "http://localhost:3000",
+   apiUrlExtern: "http://192.168.1.67:3000/api",
+   apiUrlFile: "http://192.168.1.67:3000",
+   // apiUrlFile: "http://localhost:3000",
   // apiUrlExtern: "https://broker-passport-screen.ngrok-free.dev",
   // apiUrlExtern: "http://localhost:3000",
-   apiUrlExtern: "http://localhost:3000/api",
+   // apiUrlExtern: "http://localhost:3000/api",
   apiUrlIntern: "http://localhost:3000"
 };
 
